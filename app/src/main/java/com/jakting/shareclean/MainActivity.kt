fd2e6382ab3d903853c4c2fec10acfea1d17d80b
hjthjthjt <i@jakting.com>
2020-07-26 14:33:22 +0800

    (unfinished):prepare for misc.

--- app/src/main/java/com/jakting/shareclean/MainActivity.kt
+++ app/src/main/java/com/jakting/shareclean/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.jakting.shareclean
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.jakting.shareclean.utils.logd
-import com.jakting.shareclean.utils.toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.topjohnwu.superuser.Shell
 import com.topjohnwu.superuser.ShellUtils.isValidOutput
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var isWorked = false //模块是否被检测到正常工作
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!getDarkModeStatus(this)) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
         setContentView(R.layout.activity_main)
+        init()
         setModuleStatusCard()
+    }
+
+    private fun init(){
+        app_manage_card.setOnClickListener(this)
     }
 
     private fun setModuleStatusCard() {
@@ -30,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                 riru_status_card_title.text = getString(R.string.riru_status_card_exist)
                 riru_status_card_desc.text = getString(R.string.riru_status_card_exist_detail)
                 riru_status_card_icon.setImageResource(R.drawable.ic_baseline_check_circle_24)
-            } else{
+            } else {
                 //Riru - IFW Enhance 未生效
                 riru_status_card.setCardBackgroundColor(resources.getColor(R.color.colorRed))
                 riru_status_card_title.text = getString(R.string.riru_status_card_not)
@@ -41,5 +54,32 @@ class MainActivity : AppCompatActivity() {
 //        else {
 //            //未授予 Root 权限 & 未 Root
 //        }
+    }
+
+    private fun getDarkModeStatus(context: Context): Boolean {
+        val mode: Int =
+            context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return mode == Configuration.UI_MODE_NIGHT_YES
+    }
+
+    private fun goAppManage() {
+        val intent = Intent(this, AppListActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0!!.id) {
+            R.id.app_manage_card -> {
+                if (!isWorked) {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle(R.string.app_manage_dialog_title)
+                        .setMessage(R.string.app_manage_dialog_desc)
+                        .setPositiveButton(R.string.dialog_positive) { dialog, which ->
+                            goAppManage()
+                        }
+                        .show()
+                }
+            }
+        }
     }
 }

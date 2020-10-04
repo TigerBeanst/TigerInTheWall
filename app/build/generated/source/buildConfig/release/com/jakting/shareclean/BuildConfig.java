d6a8307c259dd1b23c2f2977e5fcb10ab687be87
hjthjthjt <i@jakting.com>
2020-10-04 11:55:14 +0800

    update versionCode

--- app/build/generated/source/buildConfig/release/com/jakting/shareclean/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/jakting/shareclean/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.jakting.shareclean";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 20200913;
-  public static final String VERSION_NAME = "1.0.1";
+  public static final int VERSION_CODE = 20201001;
+  public static final String VERSION_NAME = "1.0.2";
 }

064e8bfcf0b19e8f9fab737c1f3fb9f6aa1ac517
hjthjthjt <i@jakting.com>
2022-02-21 23:09:49 +0800

    add: useful Kotlin code

--- app/src/main/java/com/jakting/shareclean/utils/ext.kt
+++ app/src/main/java/com/jakting/shareclean/utils/ext.kt
@@ -3,10 +3,18 @@ package com.jakting.shareclean.utils
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.jakting.shareclean.R
 import com.jakting.shareclean.utils.MyApplication.Companion.appContext
-import com.topjohnwu.superuser.Shell
+
+
+@ColorInt
+fun Context.themeColor(@AttrRes attrRes: Int): Int = TypedValue()
+    .apply { theme.resolveAttribute (attrRes, this, true) }
+    .data
 
 fun getManageTypeTitle(tag: String): String {
     return when (tag) {

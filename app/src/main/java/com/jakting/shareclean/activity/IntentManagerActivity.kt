a529e53871adcf15312ddd614b8f10e5820f4f81
hjthjthjt <i@jakting.com>
2022-03-24 02:19:33 +0800

    add: ui for "Clean Manager"

--- app/src/main/java/com/jakting/shareclean/activity/IntentManagerActivity.kt
+++ app/src/main/java/com/jakting/shareclean/activity/IntentManagerActivity.kt
@@ -2,28 +2,38 @@ package com.jakting.shareclean.activity
 
 import android.os.Bundle
 import android.view.Window
+import android.widget.ImageView
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import com.drake.brv.utils.BRV
+import com.drake.brv.utils.linear
+import com.drake.brv.utils.setup
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.transition.platform.MaterialArcMotion
 import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
+import com.jakting.shareclean.BR
 import com.jakting.shareclean.BaseActivity
 import com.jakting.shareclean.R
-import com.jakting.shareclean.databinding.ActivityIntentManagerBinding
+import com.jakting.shareclean.data.App
+import com.jakting.shareclean.data.AppInfo
+import com.jakting.shareclean.data.IntentType
+import com.jakting.shareclean.databinding.ActivityCleanManagerBinding
+import com.jakting.shareclean.utils.getAppIconByPackageName
 
 class IntentManagerActivity : BaseActivity() {
 
-    private lateinit var binding: ActivityIntentManagerBinding
+    private lateinit var binding: ActivityCleanManagerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
-        binding = ActivityIntentManagerBinding.inflate(layoutInflater)
+        binding = ActivityCleanManagerBinding.inflate(layoutInflater)
         initView()
+        initData()
         setContentView(binding.root)
         super.onCreate(savedInstanceState)
     }
 
-    private fun initView(){
+    private fun initView() {
         setEnterSharedElementCallback(MaterialContainerTransformSharedElementCallback())
         window.sharedElementEnterTransition = MaterialContainerTransform().apply {
             addTarget(R.id.activity_layout)
@@ -39,5 +49,35 @@ class IntentManagerActivity : BaseActivity() {
         }
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initData() {
+        BRV.modelId = BR.app
+//        val intent = AppIntent("send", "aaa")
+//        val app = App(
+//            appName = "测试dsadsadsadasffdsfsa啊啊啊啊啊啥啥啥",
+//            packageName = "com.tencent.mm",
+//            intentList = listOf(intent)
+//        )
+//        val dataaa = ArrayList<App>()
+//        dataaa.add(app)
+//        dataaa.add(app)
+//        dataaa.add(app)
+//        dataaa.add(app)
+        val appList = AppInfo(
+            IntentType(
+                share = true,
+                view = true,
+                text = true,
+                browser = true
+            )
+        ).getAppList()
+        binding.managerCleanRecyclerView.linear().setup {
+            addType<App>(R.layout.item_manager_clean)
+            onBind {
+                val appIcon = findView<ImageView>(R.id.app_icon)
+                appIcon.setImageDrawable(getAppIconByPackageName(getModel<App>().packageName))
+            }
+        }.models = appList
     }
 }
